f7680c859962cdbfdbaa3b1da3cf6f42ef2cdc21
bob <zoudino@gmail.com>
2019-01-03 10:49:51 -0500

    debug the module issue

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from apiclient.discovery import build, build_from_document
 from flask import Flask, render_template, request, json, make_response
 from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
-from connect_dialogflow import dialog_config
+from connect_dialogflow.dialog_config import detect_intent_texts
 import dialogflow
 import os
 
